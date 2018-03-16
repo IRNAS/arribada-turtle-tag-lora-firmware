c759325eb81cdaaec2e7ecf6adbefca3727cddd9
Joshua Booth <joshua@icoteq.com>
2018-03-16 16:51:37 +0000

    define configuration tags

--- core/config_if/config_if.h
+++ core/config_if/config_if.h
@@ -50,6 +50,60 @@ typedef struct
 
 typedef enum
 {
+    // GPS
+    CONFIG_IF_TAG_GPS_LOG_POSITION_ENABLE = 0x0000, // Enable logging of position information.
+    CONFIG_IF_TAG_GPS_LOG_TTFF_ENABLE,              // Enable logging of time to first fix (TTFF)
+    CONFIG_IF_TAG_GPS_TRIGGER_MODE,                 // Mode shall allow for continuous operation or external switch triggered operation.
+    CONFIG_IF_TAG_GPS_UART_BAUD_RATE,               // The UART baud rate to be used for communications with the M8N GPS device.
+
+    // Real time crystal
+    CONFIG_IF_TAG_RTC_SYNC_TO_GPS_ENABLE = 0x0600, // If set, the RTC will sync to GPS time
+    CONFIG_IF_TAG_RTC_CURRENT_DATE_AND_TIME,       // Current date and time
+
+    // Logging
+    CONFIG_IF_TAG_LOGGING_ENABLE = 0x0100,              // Controls whether the global logging function is enabled or disabled.
+    CONFIG_IF_TAG_LOGGING_BYTES_WRITTEN,                // Total number of bytes used in configuration file.
+    CONFIG_IF_TAG_LOGGING_FILE_SIZE,                    // Maximum file size allowed for logging. This is set when the logging file is created.
+    CONFIG_IF_TAG_LOGGING_FILE_TYPE,                    // Indicates fill or circular mode. This is set when the logging file is created.
+    CONFIG_IF_TAG_LOGGING_GROUP_SENSOR_READINGS_ENABLE, // If set, the logging engine should attempt to group multiple sensor readings together into a single log entry.
+    CONFIG_IF_TAG_LOGGING_START_END_SYNC_ENABLE,        // If set, each log entry shall be framed with a start and end sync. This is set when the logging file is created.
+    CONFIG_IF_TAG_LOGGING_DATE_TIME_STAMP_ENABLE,       // If set, the RTC date & time shall be logged with each long entry.
+    CONFIG_IF_TAG_LOGGING_HIGH_RESOLUTION_TIMER_ENABLE, // If set, the HRT shall be logged with each long entry.
+
+    // Accelerometer
+    CONFIG_IF_TAG_AXL_LOG ENABLE = 0x0200,    // The AXL shall be enabled for logging.
+    CONFIG_IF_TAG_AXL_CONFIG,                 // G-force setting, sensitivity mode, etc.
+    CONFIG_IF_TAG_AXL_G_FORCE_HIGH_THRESHOLD, // The X2+Y2+Z2 vector magnitude threshold.
+    CONFIG_IF_TAG_AXL_SAMPLE_RATE,            // Number of times per second to perform a reading of the AXL.
+    CONFIG_IF_TAG_AXL_MODE,                   // 0=>PERIODIC, 3=>TRIGGER_ABOVE
+
+    // Pressure sensor
+    CONFIG_IF_TAG_PRESSURE_SENSOR_LOG_ENABLE = 0x0300,  // The pressure sensor shall be enabled for logging.
+    CONFIG_IF_TAG_PRESSURE_SAMPLE_RATE,                 // Number of times per second to perform a reading of the pressure sensor.
+    CONFIG_IF_TAG_PRESSURE_LOW_THRESHOLD,               // Low threshold setting.
+    CONFIG_IF_TAG_PRESSURE_HIGH_THRESHOLD,              // High threshold setting.
+    CONFIG_IF_TAG_PRESSURE_MODE,                        // 0=>PERIODIC, 1=>TRIGGER_BELOW, 2=>TRIGGER_BETWEEN, 3=>TRIGGER_ABOVE
+
+    // Temperature sensor
+    CONFIG_IF_TAG_TEMP_SENSOR_LOG_ENABLE = 0x0700,  // The sensor shall be enabled for logging.
+    CONFIG_IF_TAG_TEMP_SENSOR_SAMPLE_RATE,          // Number of times per second to perform a reading of the sensor.
+    CONFIG_IF_TAG_TEMP_SENSOR_LOW_THRESHOLD,        // Low threshold setting.
+    CONFIG_IF_TAG_TEMP_SENSOR_HIGH_THRESHOLD,       // High threshold setting.
+    CONFIG_IF_TAG_TEMP_SENSOR_MODE,                 // 0=>PERIODIC, 1=>TRIGGER_BELOW, 2=>TRIGGER_BETWEEN, 3=>TRIGGER_ABOVE
+
+    // System
+    CONFIG_IF_TAG_SYSTEM_DEVICE_IDENTIFIER = 0x0400, // Unique device identifier.
+
+    // Bluetooth
+    CONFIG_IF_TAG_BLUETOOTH_UUID = 0x0500,                      // The UUID should uniquely identify the bluetooth function.
+    CONFIG_IF_TAG_BLUETOOTH_BEACON_ENABLE,                      // Beacon function is enabled.
+    CONFIG_IF_TAG_BLUETOOTH_BEACON_GEO_FENCE_TRIGGER_LOCATION,  // Beacon function is only enabled at the specified geo-fence location.
+    CONFIG_IF_TAG_BLUETOOTH_BEACON_ADVERTISING_INTERVAL,        // The beacon advertising interval expressed in milliseconds.
+    CONFIG_IF_TAG_BLUETOOTH_BEACON_ADVERTISING_CONFIGURATION,   // TBD - for future expansion of the advertising payload to convey in the beacon.
+} config_if_tags_t;
+
+typedef enum
+{
     CONFIG_IF_USB,
     CONFIG_IF_BLE
 } config_if_backend_t;
