f75a2c9399e3ee6ab0aeef0276ae69c33f46212f
Joshua Booth <joshua@icoteq.com>
2018-07-27 17:08:41 +0100

    AG-34: improve battery monitor error handling

--- devices/battery/BQ27621/BQ27621.c
+++ devices/battery/BQ27621/BQ27621.c
@@ -23,6 +23,7 @@
 #include "syshal_i2c.h"
 #include "syshal_batt.h"
 #include "syshal_time.h"
+#include "syshal_gpio.h"
 #include "debug.h"
 #include "bsp.h"
 
@@ -30,55 +31,63 @@
 
 // Read and return the contents of the flag register
 #define ERRORVAL -1
+#define BQ27621_TIME_BETWEEN_TRANSFERS_MS (1)
+#define BQ27621_NUM_OF_ZERO_CHARGE_REQUIRED (10)
 
-static inline void BQ27621_write(uint8_t regAddress, uint16_t value)
+static uint32_t number_of_charge_levels_zero;
+
+static inline int BQ27621_write(uint8_t regAddress, uint16_t value)
 {
     uint8_t data[2];
-    data[0] = value >> 8;
-    data[1] = value & 0xFF;
-    syshal_i2c_write_reg(I2C_BATTERY, BQ27621_ADDR, regAddress, data, 2);
+    data[1] = value >> 8;
+    data[0] = value & 0xFF;
+    return syshal_i2c_write_reg(I2C_BATTERY, BQ27621_ADDR, regAddress, data, 2);
 }
 
-static inline uint16_t BQ27621_read(uint8_t regAddress)
+static inline int BQ27621_read(uint8_t regAddress, uint16_t * value)
 {
-    uint8_t data[2];
-    uint32_t length = syshal_i2c_read_reg(I2C_BATTERY, BQ27621_ADDR, regAddress, data, 2);
+    uint8_t read_bytes[2];
+    int length = syshal_i2c_read_reg(I2C_BATTERY, BQ27621_ADDR, regAddress, read_bytes, sizeof(read_bytes));
 
-    if (2 == length)
-        return (data[1] << 8) | (data[0] & 0xff);
-    else
-        return 0;
+    if (2 != length)
+        return length;
+
+    *value = (read_bytes[1] << 8) | (read_bytes[0] & 0xff);
+
+    return 0;
 }
 
-static inline uint16_t BQ27621_read_flags(void)
+static inline int BQ27621_read_flags(uint16_t * flags)
 {
-    return BQ27621_read(BQ27621_REG_FLAGS);
+    return BQ27621_read(BQ27621_REG_FLAGS, flags);
 }
 
 static int BQ27621_cfgupdate(bool active)
 {
     const uint32_t limit = 100;
     uint32_t try = limit;
-    uint16_t status;
+    uint16_t status = 0;
     uint16_t cmd = active ? BQ27621_SET_CFGUPDATE : BQ27621_SOFT_RESET;
 
     BQ27621_write(BQ27621_REG_CTRL, cmd);
 
+    BQ27621_read_flags(&status);
+
     do
     {
         syshal_time_delay_ms(25);
-        status = BQ27621_read_flags();
+        BQ27621_read_flags(&status);
     }
     while (!!(status & BQ27621_FLAG_CFGUP) != active && --try);
 
     if (!try)
     {
-        DEBUG_PR_ERROR("Timed out waiting for cfgupdate flag %d\n", active);
+        DEBUG_PR_ERROR("Timed out waiting for cfgupdate flag %d", active);
         return ERRORVAL;
     }
 
     if (limit - try > 3)
-            DEBUG_PR_WARN("Cfgupdate %d, retries %lu\n", active, limit - try);
+            DEBUG_PR_WARN("Cfgupdate %d, retries %lu", active, limit - try);
 
     return 0;
 }
@@ -87,16 +96,17 @@ static inline int BQ27621_set_cfgupdate(void)
 {
     int status = BQ27621_cfgupdate(true);
     if (ERRORVAL == status)
-        DEBUG_PR_ERROR("Bus error on set_cfgupdate: %d\n", status);
+        DEBUG_PR_ERROR("Bus error on set_cfgupdate: %d", status);
 
     return status;
 }
 
 static inline int BQ27621_soft_reset(void)
 {
+    DEBUG_PR_TRACE("%s", __FUNCTION__);
     int status = BQ27621_cfgupdate(false);
     if (ERRORVAL == status)
-        DEBUG_PR_ERROR("Bus error on soft_reset: %d\n", status);
+        DEBUG_PR_ERROR("Bus error on soft_reset: %d", status);
 
     return status;
 }
@@ -105,6 +115,11 @@ static inline int BQ27621_soft_reset(void)
 
 int syshal_batt_init(void)
 {
+    syshal_gpio_init(GPIO_GPOUT);
+    syshal_gpio_set_output_high(GPIO_GPOUT);
+
+    number_of_charge_levels_zero = 0;
+
     //BQ27621_set_cfgupdate();
     if (syshal_i2c_is_device_ready(I2C_BATTERY, BQ27621_ADDR) != SYSHAL_I2C_NO_ERROR)
     {
@@ -112,15 +127,64 @@ int syshal_batt_init(void)
         return SYSHAL_BATT_ERROR_DEVICE_UNRESPONSIVE;
     }
 
-    // Wait for battery reading to return it's first reading
-    while (syshal_batt_level() <= 0)
-    {}
+    syshal_time_delay_ms(1000);
+
+    // Issue a soft reset if the ITPOR flag is set
+    uint16_t flags = 0;
+    int error;
+    error = BQ27621_read_flags(&flags);
+    if (flags & BQ27621_FLAG_ITPOR)
+    {
+        BQ27621_soft_reset();
+        syshal_time_delay_ms(1000);
+    }
+
+    error = BQ27621_read_flags(&flags);
+
+    // If the ITPOR flag has still not been cleared then the device is not responding
+    if (flags & BQ27621_FLAG_ITPOR ||
+        error < 0)
+        return SYSHAL_BATT_ERROR_DEVICE_UNRESPONSIVE;
+
+    // Wait for device to start giving stable SOC (state of charge) readings above 0
+    uint8_t level = 0;
+    int past_level = 0;
+    uint32_t number_of_stable_levels = 0;
+
+    while (level == 0 || number_of_stable_levels < 1000)
+    {
+        syshal_time_delay_ms(1); // Give the device time between requests to start-up
+        int bytes_written = syshal_i2c_read_reg(I2C_BATTERY, BQ27621_ADDR, BQ27621_REG_STATE_OF_CHARGE, &level, sizeof(level));
+
+        if (bytes_written == sizeof(level))
+        {
+            if (level != past_level)
+            {
+                number_of_stable_levels = 0;
+            }
+            else
+            {
+                if (level > 0)
+                {
+                    number_of_stable_levels++;
+                }
+                else
+                {
+                    number_of_stable_levels = 0;
+                }
+            }
+
+            past_level = level;
+        }
+    }
+
+    DEBUG_PR_TRACE("BQ27621 init");
 
     return SYSHAL_BATT_NO_ERROR;
 }
 
 // Return the battery temperature in tenths of degree Kelvin
-uint16_t syshal_batt_temp(void)
+/*uint16_t syshal_batt_temp(void)
 {
     return BQ27621_read(BQ27621_REG_TEMP);
 }
@@ -129,7 +193,7 @@ uint16_t syshal_batt_temp(void)
 uint16_t syshal_batt_voltage(void)
 {
     return BQ27621_read(BQ27621_REG_VOLT);
-}
+}*/
 
 /**
  * @brief      Returns the percentage charge left in the battery
@@ -138,13 +202,34 @@ uint16_t syshal_batt_voltage(void)
  */
 int syshal_batt_level(void)
 {
+    uint16_t flags = 0;
     uint8_t level;
+
+    // Has the device reset for some reason?
+    int error = BQ27621_read_flags(&flags);
+
+    if (error < 0)
+        return SYSHAL_BATT_ERROR_DEVICE_UNRESPONSIVE;
+
+    if (flags & BQ27621_FLAG_ITPOR)
+    {
+        DEBUG_PR_WARN("BQ27621 has reset, FLAGS: 0x%04X", flags);
+        syshal_batt_init();
+    }
 
     int status = syshal_i2c_read_reg(I2C_BATTERY, BQ27621_ADDR, BQ27621_REG_STATE_OF_CHARGE, &level, 1);
 
+    if (0 == level)
+    {
+        number_of_charge_levels_zero++;
+        DEBUG_PR_WARN("BQ27621 zero reading");
+        if (number_of_charge_levels_zero < BQ27621_NUM_OF_ZERO_CHARGE_REQUIRED)
+            return SYSHAL_BATT_ERROR_BUSY;
+    }
+
     if (SYSHAL_I2C_ERROR_TIMEOUT == status)
         return SYSHAL_BATT_ERROR_TIMEOUT;
-    else if (status < 0)
+    else if (status <= 0)
         return SYSHAL_BATT_ERROR_DEVICE_UNRESPONSIVE;
     else
         return level;
@@ -155,17 +240,15 @@ int syshal_batt_level(void)
  *
  * @return     True if charging, false if not
  */
-bool syshal_batt_charging(void)
+/*bool syshal_batt_charging(void)
 {
-    return false;
+    uint8_t data[2];
+    syshal_i2c_read_reg(I2C_BATTERY, BQ27621_ADDR, BQ27621_REG_AVERAGE_POWER, data, 2);
 
-//    uint8_t data[2];
-//    syshal_i2c_read_reg(I2C_BATTERY, BQ27621_ADDR, BQ27621_REG_AVERAGE_POWER, data, 2);
-//
-//    int16_t averagePower = ((int16_t)data[1] << 8) | ((int16_t)data[0] & 0xff);
-//
-//    if (averagePower >= 0)
-//        return true;
-//    else
-//        return false;
-}
+    int16_t averagePower = ((int16_t)data[1] << 8) | ((int16_t)data[0] & 0xff);
+
+    if (averagePower >= 0)
+        return true;
+    else
+        return false;
+}*/
